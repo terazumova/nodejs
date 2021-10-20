7c6a1d4da0e13c3d0065843b82e04c83e7243782
terazumova <te.razumova@gmail.com>
2021-10-20 17:23:10 +0300

    fixed issues

--- first/app.js
+++ first/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const moment = require('moment');
+const { DateTime } = require("luxon");
 
-const currentLaunch = moment().locale('ru').format('DD/MM/YYYY HH:mm:ss'); 
+const currentLaunch = Date.now();
 const lastLaunch = fs.readFileSync('last-launch.txt', 'utf-8');
 
 if (!lastLaunch) {
@@ -11,10 +11,11 @@ if (!lastLaunch) {
     if (err) throw err;
   });
 } else {
-  const ms = moment(currentLaunch,"DD/MM/YYYY HH:mm:ss").diff(moment(lastLaunch,"DD/MM/YYYY HH:mm:ss"));
-  const d = moment.duration(ms);
-  
-  const diffText = (d.days() ? d.days() + ' часов ' : '') + (d.hours() ? d.hours() + ' часов ' : '') + (d.minutes() ? d.minutes() + ' минут ' : '') +  (d.seconds() ? d.seconds() + ' секунд' : '');
+  const currentLaunchParsed = DateTime.fromJSDate((new Date(currentLaunch)));
+  const lastLaunchParsed = DateTime.fromJSDate((new Date(Number(lastLaunch))));
+
+  const diffInDays = currentLaunchParsed.diff(lastLaunchParsed, ['days', 'hours', 'minutes', 'seconds']);
+  const diffText = (diffInDays.values.days ? diffInDays.values.days + ' часов ' : '') + (diffInDays.values.hours ? diffInDays.values.hours + ' часов ' : '') + (diffInDays.values.minutes ? diffInDays.values.minutes + ' минут ' : '') +  (diffInDays.values.seconds ? diffInDays.values.seconds + ' секунд' : '');
 
   console.log(`Ну ты же уже запускал меня ${diffText} тому назад`);
 }
